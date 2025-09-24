8ec9b759746722a5e1d59bb2b0f38356f90009d6
LSyaRE <jonasvillacis@yahoo.com>
2025-09-24 16:00:38 -0500

    docs: Se agrega documentacion de levantamiento de ambiente

---
+++ README.md
@@ -0,0 +1,12 @@
+# Pet Pocket 
+
+## Requisitos previos
+- Instalar nodejs
+- Mysql
+- MongoDB
+
+## Iniciar servidor 
+``` bash
+npm install
+npm run dev
+```
--- src/keys.js
+++ src/keys.js
@@ -5,6 +5,7 @@ const MYSQLDATABASE = 'Petpoket';
 const MYSQLPORT = '3306';
 const MYSQL_URI = process.env.MYSQL_URI || '';
 const MONGODB_URI = process.env.MONGODB_URI || 'mongodb+srv://leonardoedi1979:leonardo2411@edisoncloud.ux4si.mongodb.net/petpoket';
+
 // Exportar las variables de configuración
 module.exports = {
     MYSQLHOST,
