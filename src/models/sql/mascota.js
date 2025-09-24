72e3d83bcde0f42ae37f4afda3c61e25f2f7861d
David Quiroga <dqdavudquiroga@gmail.com>
2025-09-24 14:53:23 -0500

    Initial commit

--- src/models/sql/mascota.js
+++ src/models/sql/mascota.js
@@ -1,22 +1,21 @@
-   const mascota = (sequelize, type) => {
-       return sequelize.define('mascotas', {
-           idMascota: {
-               type: type.INTEGER,
-               autoIncrement: true,
-               primaryKey: true,
-           },
-           nombreMascota: type.STRING,
-           especie: type.STRING,
-           raza: type.STRING,
-           edad: type.INTEGER,
-           sexo: type.STRING,
-           idPropietario: type.INTEGER,
-           createMascota: type.STRING,
-           updateMascota: type.STRING,
-       }, {
-           timestamps: false,
-           comment: 'Tabla de Mascotas'
-       });
-   }
-   module.exports = mascota;
-   
+const mascota = (sequelize, type) => {
+    return sequelize.define('mascotas', {
+        idMascota: {
+            type: type.INTEGER,
+            autoIncrement: true,
+            primaryKey: true,
+        },
+        nombreMascota: type.STRING,
+        especie: type.STRING,
+        raza: type.STRING,
+        edad: type.INTEGER,
+        sexo: type.STRING,
+        idPropietario: type.INTEGER,
+        createMascota: type.STRING,
+        updateMascota: type.STRING,
+    }, {
+        timestamps: false,
+        comment: 'Tabla de Mascotas'
+    });
+}
+module.exports = mascota;
